371f9f02a0287f06aa9ea84dfa74d86d21bb0aa3
Maharshi Patel <102695974+Maharshi2403@users.noreply.github.com>
2025-03-10 15:35:57 -0400

    Server Created

---
+++ server.js
@@ -0,0 +1,12 @@
+const exp = require('express');
+const cors = require('cors');
+const server = exp();
+
+server.use(cors({ origin: "https://production-repo.vercel.app" }))
+
+
+server.get("/", (req,res)=> {
+    const text = req.body.test;
+    res.status(200).send(text);
+})
+
